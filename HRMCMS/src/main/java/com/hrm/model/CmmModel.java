a5b66d15ad3f6d35271d88ba29c94ada2f99a3f6
ToanTK <khanhtoantruong196@gmail.com>
2024-09-06 16:59:39 +0700

    add model

---
+++ HRMCMS/src/main/java/com/hrm/model/CmmModel.java
@@ -0,0 +1,45 @@
+package com.hrm.model;
+
+public class CmmModel {
+
+	private String dtCreate;
+
+	private String createBy;
+
+	private String dtUpdt;
+
+	private String updtBy;
+
+	public String getDtCreate() {
+		return dtCreate;
+	}
+
+	public void setDtCreate(String dtCreate) {
+		this.dtCreate = dtCreate;
+	}
+
+	public String getCreateBy() {
+		return createBy;
+	}
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+
+	public String getDtUpdt() {
+		return dtUpdt;
+	}
+
+	public void setDtUpdt(String dtUpdt) {
+		this.dtUpdt = dtUpdt;
+	}
+
+	public String getUpdtBy() {
+		return updtBy;
+	}
+
+	public void setUpdtBy(String updtBy) {
+		this.updtBy = updtBy;
+	}
+
+}
